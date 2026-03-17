12ca12d596b7444206dbc34dee777e563f0774f4
a.zakharenkov <a.zakharenkov@mkskom.ru>
2026-03-17 17:43:38 +0300

    initial commit

---
+++ src/main/java/ru/zahaand/smarttaskbot/SmartTaskBotApplication.java
@@ -0,0 +1,13 @@
+package ru.zahaand.smarttaskbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTaskBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zahaand/smarttaskbot/SmartTaskBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zahaand.smarttaskbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
